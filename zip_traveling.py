7d7031170e11a3a84359af3df223e71e3d2e5db4
Yann <yannmjl@gmail.com>
2018-10-15 01:44:39 -0600

    last commit on monday 2 am

--- zip_traveling.py
+++ zip_traveling.py
@@ -27,16 +27,18 @@
 # we are going to use a queue to store order after reading them (meaning first order in,
 # will be the first order out)
 
-# import the csv module that will help read and get data from the input files
-import csv
-from queue import PriorityQueue
+import csv                                      # csv module that will help read files
+import sched, time                              # sched module implements a general purpose event scheduler.
+from queue import PriorityQueue                 # implements priority queue
+
 
 class ZipScheduler:
 
-    order_queue = PriorityQueue()
+    # order_queue = PriorityQueue()
+    list_of_order = []
 
     def read_order(file):
-        order_list = []                         # init an empty that store order read from the csv file 
+        order_list = ZipScheduler.list_of_order                         # init an empty that store order read from the csv file 
         with open(file, 'r') as csvfile:
             reader_order = csv.reader(csvfile)  # expect values to be separated by a coma by default
             # next(reader)                      # skip the first line if we had title on the csv file
@@ -44,6 +46,33 @@ class ZipScheduler:
                 order_list.append(line)
         return order_list
 
-print(ZipScheduler.read_order('hospitals.csv'))
+    ## we are going to check for each order in the file. add order on top of the list if it has Emergency 
+    ## priority and the largest number of second(meaning an order was made earlier than an other)
+    def store_order(file):
+        orders = ZipScheduler.read_order(file)
+        for i in range(len(orders)):
+            return (orders[i])
+            #for j in range(len(orders[i])):
+                #print(orders[i][j])    
+
+    #def queue_order(received_time, hospital, priority):
+    def queue_order():
+        orders = ZipScheduler.list_of_order
+        for item in orders:
+            print(item[2])
+
+        
+    def do_something(): 
+        print("Doing stuff...")
+        # do your stuff
+        time.sleep(10)
+
+while True:
+    ZipScheduler.do_something()
+
+#ZipScheduler.store_order('orders.csv')
+#ZipScheduler.queue_order()
+# print(ZipScheduler.read_order('hospitals.csv'))
+
 
                 
